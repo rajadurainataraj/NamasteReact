dc08d9f06ec8c5f122703db613eed47c3546821c
rajadurainataraj <rajadurainatraj@gmail.com>
2023-02-08 21:31:13 +0530

    lesson3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import logo from './assets/images/logo.jpg';
+import profile from './assets/images/person.png';
 
 // let header = React.createElement('h1',{class:'header'},"Fruit Store🍒🍓")
 
@@ -16,18 +18,37 @@ const title = "hello"
 const Content = () =>{
 return "React DOM Element"
 }
-console.log(<Content/>)
-const HeaderFunction = ()=>{
+
+// const Header =()=> (
+//     <div>
+//         <h1 key="h1">"h1"</h1>
+//         <h2 key='h2'>"h2"</h2>
+//         <h3 key='h3'>"h3"</h3>
+//     </div>
+// )
+// console.log(<Content/>)
+// const HeaderFunction = ()=>{
+//     return (
+//         <div>
+//             <Header></Header>
+//              {Content()}
+//             <h1>functional render</h1>
+//         </div>
+//     )
+// }
+
+const Header =()=>{
     return (
-        <div>
-            
-             {Content()}
-            <h1>functional render</h1>
+        <div className='container'>
+            <img className='logo' src={logo}/>
+            <input className='searchBar' type='search' placeholder='search items' name='name' />
+            <img className='userIcon' src={profile} />
         </div>
     )
 }
+
 let root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeaderFunction/>)
+root.render(<Header/>)
 
 
 
